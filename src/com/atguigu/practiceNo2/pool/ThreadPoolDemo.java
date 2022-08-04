1449be6267876e5a96dc2a102f7ef84d495a3a25
zhangbingbing <397525017@qq.com>
2022-08-04 15:31:36 +0800

    :art: 线程池基本使用

---
+++ src/com/atguigu/practiceNo2/pool/ThreadPoolDemo.java
@@ -0,0 +1,25 @@
+package com.atguigu.practiceNo2.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPoolDemo {
+    public static void main(String[] args) {
+        //ExecutorService pool1 = Executors.newFixedThreadPool(5);
+        //ExecutorService pool1 = Executors.newSingleThreadExecutor();
+        ExecutorService pool1 = Executors.newCachedThreadPool();
+
+        try {
+            for (int i = 0; i < 10; i++) {
+                pool1.execute(() -> {
+                    System.out.println(Thread.currentThread().getName() + "执行任务");
+                });
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            pool1.shutdown();
+        }
+
+    }
+}
